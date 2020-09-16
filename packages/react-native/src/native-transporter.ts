377717e3ad7e7f3d274a411bb2db9aaef2b89bd0
thanhtung6824 <thanhtung6824@gmail.com>
2020-09-16 17:40:00 +0700

    update

--- packages/react-native/src/native-transporter.ts
+++ packages/react-native/src/native-transporter.ts
@@ -2,6 +2,6 @@ import { Transporter } from 'iceteaid-core';
 
 export class NativeTransporter extends Transporter {
     protected boostrap(): void {
-        console.log('boostrap application .....');
+        console.log('boostrap application ......');
     }
 }
